20eb9954a131c06d04b0b3fece9ab0d06dbdb56d
Ricardo Augusto <ricardo.augusto@wmsaude.com.br>
2023-08-23 19:12:50 -0300

    Tipos de dados e declaração de variáveis

--- fundamentos_dart/meu_app/bin/meu_app.dart
+++ fundamentos_dart/meu_app/bin/meu_app.dart
@@ -1,5 +1,28 @@
-import 'package:meu_app/meu_app.dart' as meu_app;
-
 void main(List<String> arguments) {
-  print('Hello world: ${meu_app.calculate()}!');
+  String texto = "DIO";
+  var texto2 = "DIO";
+  String texto3;
+
+  int numero1 = 1;
+  var numero2 = 2;
+  int numero3;
+
+  List<String> lista = [];
+  lista.add("A");
+
+  var lista2 = [];
+  lista2.add(2);
+  lista2.add("A");
+  lista2.add(1.2);
+
+  int numero4 = 11;
+  var numero5 = 10;
+
+  print("Retorna verdadeiro se e somente se esse numero for par");
+  print(numero4.isEven);
+  print(numero5.isEven);
+
+  print("Retorna verdadeiro se e somente se esse numero for impar");
+  print(numero4.isOdd);
+  print(numero5.isOdd);
 }
