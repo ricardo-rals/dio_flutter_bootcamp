d6f2cbe73e56cfb63993942110c0fed2481446e6
Ricardo Augusto <ricardo.augusto@wmsaude.com.br>
2023-08-28 23:21:47 -0300

    Tipos de dados String

--- fundamentos_dart/meu_app/bin/meu_app.dart
+++ fundamentos_dart/meu_app/bin/meu_app.dart
@@ -44,43 +44,85 @@ void main(List<String> arguments) {
   // // print(int.parse("teste"));
   // print(int.tryParse("teste"));
 
-  double numero1 = 10.9;
-  var numero2 = 11.1;
+  // double numero1 = 10.9;
+  // var numero2 = 11.1;
 
-  print("Remova o ponto flutuante");
-  print(numero1.truncate());
-  print(numero2.truncate());
+  // print("Remova o ponto flutuante");
+  // print(numero1.truncate());
+  // print(numero2.truncate());
 
-  print("Converte para inteiro");
-  print(numero1.toInt());
-  print(numero2.toInt());
+  // print("Converte para inteiro");
+  // print(numero1.toInt());
+  // print(numero2.toInt());
 
-  print("Arredonda para cima.");
-  print(numero1.ceil());
-  print(numero2.ceil());
+  // print("Arredonda para cima.");
+  // print(numero1.ceil());
+  // print(numero2.ceil());
 
-  print("Arredonda para baixo.");
-  print(numero1.floor());
-  print(numero2.floor());
+  // print("Arredonda para baixo.");
+  // print(numero1.floor());
+  // print(numero2.floor());
 
-  print("Retorna se o número é finito.");
-  print(numero1.isFinite);
-  print(numero2.isFinite);
+  // print("Retorna se o número é finito.");
+  // print(numero1.isFinite);
+  // print(numero2.isFinite);
 
-  print("Retorna se o número é initinito.");
-  print(numero1.isInfinite);
-  print(numero2.isInfinite);
+  // print("Retorna se o número é initinito.");
+  // print(numero1.isInfinite);
+  // print(numero2.isInfinite);
 
-  print("Retorna se o número não é válido.");
-  print(numero1.isNaN);
-  print(numero2.isNaN);
+  // print("Retorna se o número não é válido.");
+  // print(numero1.isNaN);
+  // print(numero2.isNaN);
 
-  print("Retorna se o número é negativo.");
-  print(numero1.isNegative);
-  print((numero2 * -1).isNegative);
+  // print("Retorna se o número é negativo.");
+  // print(numero1.isNegative);
+  // print((numero2 * -1).isNegative);
 
-  print("Converte String para double");
-  print(double.parse("10.09"));
-  // print(double.parse("teste"));
-  print(double.tryParse("teste"));
+  // print("Converte String para double");
+  // print(double.parse("10.09"));
+  // // print(double.parse("teste"));
+  // print(double.tryParse("teste"));
+
+  String texto1 = "Dio";
+  var texto2 = "Trilha de Flutter e Dart na DIO";
+
+  print(texto1);
+  print(texto2);
+
+  //Comparação se vazio
+  print(texto1.isEmpty);
+  print("".isEmpty);
+  print(" ".isEmpty);
+
+  // Obtem o tamanho da String
+  print(texto1.length);
+
+  // Conversão Maiusculo / Minusculo
+  print(texto1.toUpperCase());
+  print(texto2.toLowerCase());
+
+  // Se uma string contem na outra
+  print(texto2.contains(texto1.toUpperCase()));
+
+  //obtem parte da string
+  print(texto2.substring(5));
+  print(texto2.substring(1, 5));
+
+  // obtem a posição de um texto em uma string
+  print(texto2.indexOf("Flutter"));
+  print(texto2.indexOf("T"));
+  print(texto2.indexOf("@"));
+
+  // substitiu uma string em outra
+  print(texto2.replaceAll("a", "@"));
+
+  // quebra uma string por um caracter especifico
+  print(texto2.split("a"));
+  print("NOME;ENEREÇO;CEP".split(";"));
+
+  // remove espaços
+  print(" dio ".trim());
+  print(" dio ".trimLeft());
+  print(" dio ".trimRight());
 }
