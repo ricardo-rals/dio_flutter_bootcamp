c0c7bee49ef8f5287d86c6ff28749177c2e716ce
Ricardo <ricardo.rals.dev@gmail.com>
2023-10-21 18:38:25 -0300

    criando um arquivo utils para adicionar funções e que são usadas em outros arquivos, e criando um codigo para acumular numeros e somar

--- fundamentos_dart/meu_app/lib/functions/calculadora.dart
+++ fundamentos_dart/meu_app/lib/functions/calculadora.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
-import 'dart:convert';
+import 'package:meu_app/functions/utils.dart' as utils;
 
 void main(List<String> arguments) {
-  var numero1 = lerConsoleDouble("Informe o primeiro número: ");
-  var numero2 = lerConsoleDouble("Informe o segundo número: ");
-  var operacao = lerConsole("Informe a operação matemática (+, -, *, /): ");
+  var numero1 = utils.lerConsoleDouble("Informe o primeiro número: ");
+  var numero2 = utils.lerConsoleDouble("Informe o segundo número: ");
+  var operacao =
+      utils.lerConsole("Informe a operação matemática (+, -, *, /): ");
   calcular(operacao, numero1, numero2);
 }
 
@@ -12,16 +13,16 @@ void calcular(String operacao, double numero1, double numero2) {
   double resultado = 0;
   switch (operacao) {
     case "+":
-      resultado = soma(numero1, numero2);
+      resultado = utils.soma(numero1, numero2);
       break;
     case "-":
-      resultado = subtracao(numero1, numero2);
+      resultado = utils.subtracao(numero1, numero2);
       break;
     case "*":
-      resultado = multiplicacao(numero1, numero2);
+      resultado = utils.multiplicacao(numero1, numero2);
       break;
     case "/":
-      resultado = divisao(numero1, numero2);
+      resultado = utils.divisao(numero1, numero2);
       break;
     default:
       print("Operação inválida");
@@ -29,41 +30,4 @@ void calcular(String operacao, double numero1, double numero2) {
   }
   print("Operação solicitada: $operacao");
   print("O resultado da operação é: $resultado");
-}
-
-double lerConsoleDouble(String texto) {
-  var numero = double.tryParse(lerConsole(texto));
-  if (numero == null) {
-    print("Número informado incorreto, alterando para 0");
-    return 0.0;
-  } else {
-    return numero;
-  }
-}
-
-String lerConsole(String texto) {
-  print(texto);
-  var line = stdin.readLineSync(encoding: utf8);
-  return line ?? "";
-}
-
-double soma(double numero1, double numero2) {
-  return numero1 + numero2;
-}
-
-double subtracao(double numero1, double numero2) {
-  return numero1 - numero2;
-}
-
-double multiplicacao(double numero1, double numero2) {
-  return numero1 * numero2;
-}
-
-double divisao(double numero1, double numero2) {
-  if (numero2 == 0) {
-    print("Não é possivel dividir por 0");
-    exit(0);
-  } else {
-    return numero1 / numero2;
-  }
 }
